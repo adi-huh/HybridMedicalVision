148681f0ad85aeb5e4b29d8247bb08d05a19a44e
adi-huh <raiaditya126@gmail.com>
2025-10-31 00:07:06 +0530

    Delete convert_model.py

--- convert_model.py
+++
@@ -1,6 +0,0 @@
-import torch
-from safetensors.torch import save_file
-
-checkpoint = torch.load('models/medical_best_model.pth')
-save_file(checkpoint, 'models/medical_best_model.safetensors')
-print("Model converted successfully!")
